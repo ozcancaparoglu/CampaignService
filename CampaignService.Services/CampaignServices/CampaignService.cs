098638a6a508c914adb18081d2582330e5efed0b
ozcan.caparoglu <ozcan.caparoglu@gmail.com>
2020-11-23 15:43:27 +0300

    Product example

--- CampaignService.Services/CampaignServices/CampaignService.cs
+++ CampaignService.Services/CampaignServices/CampaignService.cs
@@ -15,6 +15,7 @@ namespace CampaignService.Services.CampaignServices
         private readonly IAutoMapperConfiguration autoMapper;
 
         private readonly IGenericRepository<CampaignService_Campaigns> campaignRepo;
+        private readonly IGenericRepository<Product> productRepo;
 
         public CampaignService(IUnitOfWork unitOfWork, IAutoMapperConfiguration autoMapper)
         {
@@ -22,12 +23,16 @@ namespace CampaignService.Services.CampaignServices
             this.autoMapper = autoMapper;
 
             campaignRepo = this.unitOfWork.Repository<CampaignService_Campaigns>();
+            productRepo = this.unitOfWork.Repository<Product>();
         }
 
         #region Methods
 
         public async Task<ICollection<CampaignModel>> GetAllActiveCampaigns()
         {
+            var list = new List<int> { 41, 15, 23 };
+            var products = await productRepo.FindAllAsync(x => list.Contains(x.Id));
+
             var entityList = await campaignRepo.FindAllAsync(x => x.IsActive == true);
 
             return autoMapper.MapCollection<CampaignService_Campaigns, CampaignModel>(entityList).ToList();
